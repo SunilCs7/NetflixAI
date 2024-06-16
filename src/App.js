499424c1b11335f0548c33446a732c36a9c1ef92
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-06-17 00:01:19 +0530

    Initial file upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-800">
+      hello raja dardling u are the goat
+    </div>
+  );
+}
+
+export default App;
