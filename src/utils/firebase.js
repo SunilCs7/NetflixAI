d74d6c8c7f9a5849ef390d4c332d3357ad323117
Sunil Prasad Yadav <sunilpro137@gmail.com>
2024-06-19 21:49:48 +0530

    using firebase for backend and set up all things

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4b9wNTevUGCuM1p3Sfc6_c9nD7Fl3EUA",
+  authDomain: "netflix-ai-986f0.firebaseapp.com",
+  projectId: "netflix-ai-986f0",
+  storageBucket: "netflix-ai-986f0.appspot.com",
+  messagingSenderId: "1078690851163",
+  appId: "1:1078690851163:web:14b2644c47df6f896a127e",
+  measurementId: "G-0FNGFFH576",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
